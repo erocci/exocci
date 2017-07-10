cc80bbe4a07dcb95da1c11146d6545283e681f6d
Jean Parpaillon <jean.parpaillon@free.fr>
2017-07-10 12:03:25 +0200

    Change test Model module

--- test/model_attributes_test.exs
+++ test/model_attributes_test.exs
@@ -31,18 +31,15 @@ defmodule ModelAttributesTest do
     end
   end
 
-  defmodule TestModel do
+  defmodule ModelAttributesModel do
     use OCCI.Model
 
     kind "http://example.org/occi#kind0",
-      parent: "http://schemas.ogf.org/occi/core#resource" do
-      attribute "attr0",
-	default: 10,
-	type: OCCI.Types.Integer
-
-      attribute "attr1",
-	type: [:un, :deux, :trois]
-    end
+      parent: "http://schemas.ogf.org/occi/core#resource",
+      attributes: [
+	attr0: [default: 10, type: OCCI.Types.Integer],
+	attr1: [type: [:un, :deux, :trois]]
+      ]
 
     mixin "http://example.org/occi#mixin0" do
       attribute "attr0",
@@ -60,23 +57,23 @@ defmodule ModelAttributesTest do
       depends: ["http://example.org/occi#mixin1"]
   end
 
-  test "Enum attributes" do 
+  test "Enum attributes" do
     assert match?(%{ attributes: %{ attr1: :trois }},
-      TestModel.new("http://example.org/occi#kind0", %{ id: "/an_id", attr1: :trois}))
+      ModelAttributesModel.new("http://example.org/occi#kind0", %{ id: "/an_id", attr1: :trois}))
     
     assert_raise OCCI.Error, ~r/^Invalid value:.*$/, fn ->
-      TestModel.new("http://example.org/occi#kind0", %{ id: "/an_id", attr1: :quatre})
+      ModelAttributesModel.new("http://example.org/occi#kind0", %{ id: "/an_id", attr1: :quatre})
     end
   end
 
   test "Default attribute values" do
-    res = TestModel.new("http://example.org/occi#kind0", %{ id: "/an_id" })
+    res = ModelAttributesModel.new("http://example.org/occi#kind0", %{ id: "/an_id" })
     assert match?(10, OCCI.Model.Core.Resource.get(res, "attr0"))
   end
 
   test "Default value with(out) mixins" do
     # Default value without mixin
-    res = TestModel.new("http://example.org/occi#kind0", %{ id: "/an_id" })
+    res = ModelAttributesModel.new("http://example.org/occi#kind0", %{ id: "/an_id" })
     assert match?(10, OCCI.Model.Core.Resource.get(res, "attr0"))
 
     # Default value with mixin
@@ -86,6 +83,5 @@ defmodule ModelAttributesTest do
     # Default value with mixin deps
     res = OCCI.Model.Core.Entity.add_mixin(res, "http://example.org/occi#mixin2")
     assert match?(12, OCCI.Model.Core.Resource.get(res, "attr0"))
-
   end
 end
